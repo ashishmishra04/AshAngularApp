9d70a55fcec458cc1aae99a85f05faf1bc705bac
Ashish Mishra <ashishmishra04@gmail.com>
2017-02-14 10:47:03 -0700

    Test Checkin 2

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ var app_module_1 = require('./app/app.module');
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule)
     .then(function (success) { return console.log("Bootstrap success"); })
     .catch(function (err) { return console.error(err); });
+	
+	
+	
+	
 /*
 Copyright 2016 JohnPapa.net, LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
